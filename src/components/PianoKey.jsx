21435db9cc1f1b329501d0137a0e75f63f5a51e5
TiannaLopes <tiannaalinalopes@gmail.com>
2024-01-22 21:08:32 -0500

    Adding animation

--- src/components/PianoKey.jsx
+++ src/components/PianoKey.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { usePianoKeys } from '../PianoKeysContext';
+import gsap from 'gsap';
 
 const PianoKey = ({ note, type, onClick }) => {
+  const keyRef = useRef(null);
   const { activeKeys } = usePianoKeys();
   const isActive = activeKeys.includes(note);
   const keyClassName = `piano-key ${type === 'black' ? 'black-key' : 'white-key'} ${isActive ? 'active' : ''}`;
 
+  const handleClick = () => {
+    // Trigger any onClick functionality
+    onClick(note);
+
+    // Animate the key using GSAP
+    gsap.to(keyRef.current, {
+      scale: 0.9,
+      duration: 0.1,
+      ease: "power1.out",
+      onComplete: () => {
+        gsap.to(keyRef.current, { scale: 1, duration: 0.1, ease: "power1.in" });
+      }
+    });
+  };
+
   return (
-    <div className={keyClassName} onClick={() => onClick(note)}>
+    <div ref={keyRef} className={keyClassName} onClick={handleClick}>
       <span className="key-label">{note}</span>
     </div>
   );
